a5ed9dce46778dea1949374427ef8f1e3fa02480
izqcool <584942555@qq.com>
2018-08-28 23:31:09 +0800

    add scss file support

--- src/components/Pie/Pie.js
+++ src/components/Pie/Pie.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import * as styles from './Pie.css';
+import * as d3 from 'd3';
+import * as styles from './Pie.scss';
+
 export class Pie extends React.Component {
 
+  static propTypes = {
+    width: PropTypes.string.isRequired,
+    height: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    width: '500px',
+    height: '400px'
+  };
+
+  constructor(props) {
+    super(props);
+    this.renderPie = this.renderPie.bind(this);
+
+  }
+
+  componentDidMount() {
+    const testData = [
+      {value: 200, name: 'test1'},
+      {value: 50, name: 'test2'},
+      {value: 70, name: 'test3'},
+      {value: 40, name: 'test4'}
+    ];
+    console.log(d3.pie().value(d => d.value)(testData));
+
+    this.renderPie();
+  }
+
+
+  renderPie() {
+    // clear canvas
+    const {width,height} = this.props;
+    d3.select(`.${styles.container}`).selectAll().remove();
+    console.log(d3.select(`.${styles.container}`));
+    const svg  = d3.select(`.${styles.container}`)
+      .append('svg')
+      .attr('width',width)
+      .attr('height',height);
+    console.log(svg);
+
+    const g = svg.append('g');
+
+  }
+
+
   render() {
+    const {width, height}  = this.props;
     return (
-        <div className={styles.Pie_container}>
-          12312321412
+        <div className={styles.container} style={{width:width,height:height}}>
         </div>
     )
   }
--- src/demo.js
+++ src/demo.js
@@ -2,27 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Pie} from './index';
 
-const root = document.querySelector('#app');
-console.log(root);
-
-// document.addEventListener('DOMContentLoaded',function() {
-//   const container = document.getElementById('app');
-//   if(container) {
-//     ReactDOM.render(vDomTree,container);
-//   }else {
-//     console.warn('cannot find the container');
-//   }
-// });
-
 document.addEventListener('DOMContentLoaded',function() {
   const container = document.getElementById('app');
   if(container) {
-    ReactDom.render(<Pie/>,container);
+    ReactDOM.render(<Pie/>,container);
   }else {
     console.warn('cannot find the container');
   }
 });
-// setTimeout(()=>{
-//   const root = document.querySelector('#app');
-//   ReactDOM.render(<Pie/>,root);
-// },1000);
